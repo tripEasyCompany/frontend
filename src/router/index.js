b33fc60ffff1a072f8ea1c3a89467f28b320a4fb
su-mao-xiong <sumaoxiong3963@gmail.com>
2025-05-04 22:16:15 +0800

    vue router edit

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,71 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      children: [
+        //首頁
+        {
+          path: '',
+          name: 'home',
+          component: () => import('../views/HomeView.vue'),
+        },
+        //購物專區
+        {
+          path: 'shopping',
+          name: 'shopping',
+          component: () => import('../views/front/shopping.vue'),
+        },
+        //搜索頁面
+        {
+          path: 'search',
+          name: 'search',
+          component: () => import('../views/front/search.vue'),
+        },
+      ],
+    },
+    //會員功能
+    {
+      //會員中心
+      path: '/admin',
+      name: 'member',
+      component: () => import('../views/admin/member.vue'),
+      children: [
+        //登入
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import('../views/admin/login.vue'),
+        },
+        //註冊
+        {
+          path: 'register',
+          name: 'register',
+          component: () => import('../views/admin/register.vue'),
+        },
+        //忘記密碼
+        {
+          path: 'forgot',
+          name: 'forgot',
+          component: () => import('../views/admin/forgot.vue'),
+        },
+        //會員資訊
+        {
+          path: 'member',
+          name: 'member',
+          component: () => import('../views/admin/member.vue'),
+        },
+        //收藏
+        {
+          path: 'collect',
+          name: 'collect',
+          component: () => import('../views/admin/collect.vue'),
+        },
+        //個人旅遊訂單
+        {
+          path: 'personalOrder',
+          name: 'personalOrder',
+          component: () => import('../views/admin/personalOrder.vue'),
+        },
+      ],
     },
     {
       path: '/about',
