f99a48e35e0a52512ef857f327d1ee0bad5569b0
WuPeiTing0919 <petty09190@gmail.com>
2025-05-06 03:12:17 +0800

    新增首頁測試頁面
    提供給 Render 渲染連結

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,28 @@
-<script setup>
+<template>
+    <div class="wrap">
+        <!-- Header -->
+        <HeaderComponent />
+
+        <!-- Content -->
+        <div class="content">
+            
+        </div>
+
+        <!-- Footer -->
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '@/components/HeaderComponent.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+
+export default {
+  name: 'login',
+  components: { HeaderComponent, FooterComponent }
+}
 </script>
 
-<template>
-    <h1>這是首頁</h1>
-    <span><router-link to="/admin/login">登入頁面</router-link></span>
-</template>
+<style scoped>
+
+</style>
