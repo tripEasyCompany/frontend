2a08a808be35920bc0e628f7b5ed51033c294337
PennyCY <pennylearningmore@gmail.com>
2025-06-13 20:49:33 +0800

    修改客服中心頁面

--- src/views/admin/manage/serviceSetting.vue
+++ src/views/admin/manage/serviceSetting.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="about">
-    <h1>這是客服中心設定頁面</h1>
+  <div class="wrap">
+    <!-- Header -->
+    <HeaderComponent />
+
+    <!-- Main Content -->
+    <div class="content">
+      <div class="about">
+        <h1>這是客服中心設定頁面!</h1>
+      </div>
+
+    <!-- Footer -->
+    <FooterComponent />
   </div>
 </template>
 
